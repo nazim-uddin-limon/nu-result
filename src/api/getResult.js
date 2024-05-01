7bf638aaa3ba6b6c809bbb9856cfc399473ff542
nazim-uddin-limon <developernazim.dev@gmail.com>
2024-05-01 14:21:33 +0600

    remove http protocol

--- src/api/getResult.js
+++ src/api/getResult.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const URL =
-  "http://results.nu.ac.bd/honours/first_year_result_show.php?roll_number=&reg_no=21228242413&exam_year=2022";
-
+  "//results.nu.ac.bd/honours/first_year_result_show.php?roll_number=&reg_no=21228242413&exam_year=2022";
 
 const getResult = async (url = URL) => {
     const result = await axios.get(url)
@@ -10,4 +9,10 @@ const getResult = async (url = URL) => {
     return result
 }
 
-export default getResult
+// const getResult = (url = URL) => {
+//   fetch(url).then((response) => {
+//     console.log(response);
+//   });
+// };
+
+export default getResult;
